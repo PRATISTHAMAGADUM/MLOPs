0aa3c0f0af05b61cf371dda8eb6c62e8ce04fa87
Pratiksha Magadum <pratikshamagadum67@gmail.com>
2026-04-16 14:26:26 +0530

    Added loop for continuous usage

--- train.py
+++ train.py
@@ -1,6 +1,20 @@
-try:
+while True:
+    choice = input("Choose operation (1/2/3/4): ")
+    
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
-except:
-    print("Invalid input!")
-    exit()
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid choice!")
+
+    cont = input("Continue? (y/n): ")
+    if cont == 'n':
+        break
